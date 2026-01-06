4f0f35a0cdf1454ab8e2d496d5067b23481d3399
akanksha.s.phalke <phalkeakanksha106@gmail.com>
2026-01-07 00:00:07 +0530

    Day 1: Initial Node.js setup and app.js

---
+++ Day1/app.js
@@ -0,0 +1,7 @@
+const os = require('os'); 
+console.log("Checking your system...");
+console.log("Operating System:", os.platform());
+console.log("Hello from Node.js!");
+
+const colors = require('colors');
+console.log("Node is running and it looks beautiful!".rainbow);
